4873057d9072514aeb4b2e96aa49ee619ec969ff
Foxinatel <nathanielma5on@yahoo.com>
2024-02-05 18:11:04 +0000

    Spoof hands and arms with specific coords

--- src/lib.rs
+++ src/lib.rs
@@ -135,93 +135,98 @@ fn setup(
 }
 
 fn spoof_xr_components(mut commands: Commands) {
-    commands.spawn((Transform::default(), OpenXRLeftEye));
-    commands.spawn((Transform::default(), OpenXRRightEye));
+    commands.spawn((Transform::from_xyz(1.0, 2.0, 1.1), OpenXRLeftEye));
+    commands.spawn((Transform::from_xyz(1.0, 2.0, 0.9), OpenXRRightEye));
 
-    let mut define = |ty: HandBone| {
+    let mut define_l = |ty: HandBone| {
         commands
-            .spawn((Transform::default(), OpenXRTracker, ty))
+            .spawn((Transform::from_xyz(1.0, 1.0, 1.5), OpenXRTracker, ty))
             .id()
     };
 
-    let hands = HandsResource {
-        left: HandResource {
-            palm: define(HandBone::Palm),
-            wrist: define(HandBone::Wrist),
-            thumb: ThumbResource {
-                metacarpal: define(HandBone::ThumbMetacarpal),
-                proximal: define(HandBone::ThumbProximal),
-                distal: define(HandBone::ThumbDistal),
-                tip: define(HandBone::ThumbTip),
-            },
-            index: IndexResource {
-                metacarpal: define(HandBone::IndexMetacarpal),
-                proximal: define(HandBone::IndexProximal),
-                intermediate: define(HandBone::IndexProximal),
-                distal: define(HandBone::IndexDistal),
-                tip: define(HandBone::IndexTip),
-            },
-            middle: MiddleResource {
-                metacarpal: define(HandBone::MiddleMetacarpal),
-                proximal: define(HandBone::MiddleProximal),
-                intermediate: define(HandBone::MiddleProximal),
-                distal: define(HandBone::MiddleDistal),
-                tip: define(HandBone::MiddleTip),
-            },
-            ring: RingResource {
-                metacarpal: define(HandBone::RingMetacarpal),
-                proximal: define(HandBone::RingProximal),
-                intermediate: define(HandBone::RingProximal),
-                distal: define(HandBone::RingDistal),
-                tip: define(HandBone::RingTip),
-            },
-            little: LittleResource {
-                metacarpal: define(HandBone::LittleMetacarpal),
-                proximal: define(HandBone::LittleProximal),
-                intermediate: define(HandBone::LittleProximal),
-                distal: define(HandBone::LittleDistal),
-                tip: define(HandBone::LittleTip),
-            },
+    let left = HandResource {
+        palm: define_l(HandBone::Palm),
+        wrist: define_l(HandBone::Wrist),
+        thumb: ThumbResource {
+            metacarpal: define_l(HandBone::ThumbMetacarpal),
+            proximal: define_l(HandBone::ThumbProximal),
+            distal: define_l(HandBone::ThumbDistal),
+            tip: define_l(HandBone::ThumbTip),
         },
-        right: HandResource {
-            palm: define(HandBone::Palm),
-            wrist: define(HandBone::Wrist),
-            thumb: ThumbResource {
-                metacarpal: define(HandBone::ThumbMetacarpal),
-                proximal: define(HandBone::ThumbProximal),
-                distal: define(HandBone::ThumbDistal),
-                tip: define(HandBone::ThumbTip),
-            },
-            index: IndexResource {
-                metacarpal: define(HandBone::IndexMetacarpal),
-                proximal: define(HandBone::IndexProximal),
-                intermediate: define(HandBone::IndexProximal),
-                distal: define(HandBone::IndexDistal),
-                tip: define(HandBone::IndexTip),
-            },
-            middle: MiddleResource {
-                metacarpal: define(HandBone::MiddleMetacarpal),
-                proximal: define(HandBone::MiddleProximal),
-                intermediate: define(HandBone::MiddleProximal),
-                distal: define(HandBone::MiddleDistal),
-                tip: define(HandBone::MiddleTip),
-            },
-            ring: RingResource {
-                metacarpal: define(HandBone::RingMetacarpal),
-                proximal: define(HandBone::RingProximal),
-                intermediate: define(HandBone::RingProximal),
-                distal: define(HandBone::RingDistal),
-                tip: define(HandBone::RingTip),
-            },
-            little: LittleResource {
-                metacarpal: define(HandBone::LittleMetacarpal),
-                proximal: define(HandBone::LittleProximal),
-                intermediate: define(HandBone::LittleProximal),
-                distal: define(HandBone::LittleDistal),
-                tip: define(HandBone::LittleTip),
-            },
-        }
+        index: IndexResource {
+            metacarpal: define_l(HandBone::IndexMetacarpal),
+            proximal: define_l(HandBone::IndexProximal),
+            intermediate: define_l(HandBone::IndexProximal),
+            distal: define_l(HandBone::IndexDistal),
+            tip: define_l(HandBone::IndexTip),
+        },
+        middle: MiddleResource {
+            metacarpal: define_l(HandBone::MiddleMetacarpal),
+            proximal: define_l(HandBone::MiddleProximal),
+            intermediate: define_l(HandBone::MiddleProximal),
+            distal: define_l(HandBone::MiddleDistal),
+            tip: define_l(HandBone::MiddleTip),
+        },
+        ring: RingResource {
+            metacarpal: define_l(HandBone::RingMetacarpal),
+            proximal: define_l(HandBone::RingProximal),
+            intermediate: define_l(HandBone::RingProximal),
+            distal: define_l(HandBone::RingDistal),
+            tip: define_l(HandBone::RingTip),
+        },
+        little: LittleResource {
+            metacarpal: define_l(HandBone::LittleMetacarpal),
+            proximal: define_l(HandBone::LittleProximal),
+            intermediate: define_l(HandBone::LittleProximal),
+            distal: define_l(HandBone::LittleDistal),
+            tip: define_l(HandBone::LittleTip),
+        },
     };
 
-    commands.insert_resource(hands);
+    let mut define_r = |ty: HandBone| {
+        commands
+            .spawn((Transform::from_xyz(1.0, 1.0, 0.5), OpenXRTracker, ty))
+            .id()
+    };
+
+    let right = HandResource {
+        palm: define_r(HandBone::Palm),
+        wrist: define_r(HandBone::Wrist),
+        thumb: ThumbResource {
+            metacarpal: define_r(HandBone::ThumbMetacarpal),
+            proximal: define_r(HandBone::ThumbProximal),
+            distal: define_r(HandBone::ThumbDistal),
+            tip: define_r(HandBone::ThumbTip),
+        },
+        index: IndexResource {
+            metacarpal: define_r(HandBone::IndexMetacarpal),
+            proximal: define_r(HandBone::IndexProximal),
+            intermediate: define_r(HandBone::IndexProximal),
+            distal: define_r(HandBone::IndexDistal),
+            tip: define_r(HandBone::IndexTip),
+        },
+        middle: MiddleResource {
+            metacarpal: define_r(HandBone::MiddleMetacarpal),
+            proximal: define_r(HandBone::MiddleProximal),
+            intermediate: define_r(HandBone::MiddleProximal),
+            distal: define_r(HandBone::MiddleDistal),
+            tip: define_r(HandBone::MiddleTip),
+        },
+        ring: RingResource {
+            metacarpal: define_r(HandBone::RingMetacarpal),
+            proximal: define_r(HandBone::RingProximal),
+            intermediate: define_r(HandBone::RingProximal),
+            distal: define_r(HandBone::RingDistal),
+            tip: define_r(HandBone::RingTip),
+        },
+        little: LittleResource {
+            metacarpal: define_r(HandBone::LittleMetacarpal),
+            proximal: define_r(HandBone::LittleProximal),
+            intermediate: define_r(HandBone::LittleProximal),
+            distal: define_r(HandBone::LittleDistal),
+            tip: define_r(HandBone::LittleTip),
+        },
+    };
+
+    commands.insert_resource(HandsResource { left, right });
 }
