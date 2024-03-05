a86d814c83688363c1074db696a30193341cc273
Foxinatel <nathanielma5on@yahoo.com>
2024-03-05 21:37:43 +0000

    Decouple spell casting from speech controls

--- src/speech.rs
+++ src/speech.rs
@@ -3,6 +3,10 @@ use std::sync::Arc;
 use std::{ffi::CString, path::Path};
 
 use bevy::prelude::*;
+use bevy_oxr::xr_input::{
+    hands::{common::HandsResource, HandBone},
+    trackers::OpenXRTracker,
+};
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
     SampleFormat,
@@ -10,36 +14,42 @@ use cpal::{
 use crossbeam::queue::ArrayQueue;
 use vosk::*;
 
-use crate::spell_control::{SelectedSpell, Spell, SpellStatus};
-
 const BUFFER_SIZE: usize = 10000;
-
 pub struct SpeechPlugin;
 
 #[derive(Resource)]
-pub struct SpeechRecogniser(Recognizer);
-
-#[derive(Resource)]
-pub struct VoiceBuffer {
+struct VoiceBuffer {
     queue: Arc<ArrayQueue<i16>>,
 }
+
 #[derive(Resource)]
 pub struct VoiceClip {
     data: Vec<i16>,
 }
 
+#[derive(States, Debug, Hash, Eq, PartialEq, Clone, Default)]
+pub enum RecordingStatus {
+    #[default]
+    Awaiting,
+    Recording,
+}
+
 impl Plugin for SpeechPlugin {
     fn build(&self, app: &mut App) {
         let (voice, in_stream) = setup_voice();
-        app.insert_resource(SpeechRecogniser(fetch_recogniser()))
+        app.init_state::<RecordingStatus>()
             .insert_resource(voice)
             .insert_non_send_resource(in_stream)
             .insert_resource(VoiceClip { data: Vec::new() })
-            .insert_resource(RecordingStatus {
-                just_started: false,
-                recording: false,
-                just_ended: false,
-            });
+            .add_systems(OnEnter(RecordingStatus::Recording), start_voice)
+            .add_systems(
+                Update,
+                (collect_voice, check_stop_recording).run_if(in_state(RecordingStatus::Recording)),
+            )
+            .add_systems(
+                Update,
+                (check_start_recording).run_if(in_state(RecordingStatus::Awaiting)),
+            );
     }
 }
 
@@ -83,13 +93,13 @@ fn queue_input_data(data: &[i16], queue: &Arc<ArrayQueue<i16>>) {
     }
 }
 
-fn fetch_recogniser() -> Recognizer {
-    let grammar = ["fireball", "lightning"];
-
+pub(crate) fn fetch_recogniser(grammar: &[impl AsRef<str>]) -> Recognizer {
     #[cfg(target_os = "android")]
     {
-        if !(Path::new("/storage/emulated/0/Android/data/com.github.goudanough.wizards/files/vosk-model")
-            .exists())
+        if !(Path::new(
+            "/storage/emulated/0/Android/data/com.github.goudanough.wizards/files/vosk-model",
+        )
+        .exists())
         {
             let activity = bevy::winit::ANDROID_APP.get().unwrap();
             let model_zip = activity
@@ -105,8 +115,9 @@ fn fetch_recogniser() -> Recognizer {
 
     // Attempt to fetch model, repeat until successful.
     let model: Model = loop {
-        match Model::new("/storage/emulated/0/Android/data/com.github.goudanough.wizards/files/vosk-model")
-        {
+        match Model::new(
+            "/storage/emulated/0/Android/data/com.github.goudanough.wizards/files/vosk-model",
+        ) {
             Some(model) => break model,
             None => println!("Failed to fetch vosk model, trying again."),
         }
@@ -122,14 +133,7 @@ fn fetch_recogniser() -> Recognizer {
     }
 }
 
-#[derive(Resource)]
-pub struct RecordingStatus {
-    pub just_started: bool,
-    pub recording: bool,
-    pub just_ended: bool,
-}
-
-pub fn start_voice(voice: Res<VoiceBuffer>) {
+fn start_voice(voice: Res<VoiceBuffer>) {
     let n_samples = voice.queue.len();
 
     for _ in 0..n_samples {
@@ -137,7 +141,7 @@ pub fn start_voice(voice: Res<VoiceBuffer>) {
     }
 }
 
-pub fn collect_voice(voice: Res<VoiceBuffer>, mut clip: ResMut<VoiceClip>) {
+fn collect_voice(voice: Res<VoiceBuffer>, mut clip: ResMut<VoiceClip>) {
     let n_samples = voice.queue.len();
 
     for _ in 0..n_samples {
@@ -145,31 +149,49 @@ pub fn collect_voice(voice: Res<VoiceBuffer>, mut clip: ResMut<VoiceClip>) {
     }
 }
 
-pub fn recognise_voice(
-    mut clip: ResMut<VoiceClip>,
-    mut recogniser: ResMut<SpeechRecogniser>,
-    mut next_spell_state: ResMut<NextState<SpellStatus>>,
-    mut selected_spell: ResMut<SelectedSpell>,
+fn check_start_recording(
+    hand_bones: Query<&Transform, (With<OpenXRTracker>, With<HandBone>)>,
+    hands_resource: Res<HandsResource>,
+    mut next_spell_state: ResMut<NextState<RecordingStatus>>,
 ) {
-    let mut averaged_channel_data: Vec<i16> = Vec::new();
+    let thumb_tip = hand_bones.get(hands_resource.left.thumb.tip).unwrap();
+    let index_tip = hand_bones.get(hands_resource.left.index.tip).unwrap();
+    let thumb_index_dist = (thumb_tip.translation - index_tip.translation).length();
+
+    if thumb_index_dist < 0.02 {
+        next_spell_state.set(RecordingStatus::Recording);
+    }
+}
+
+fn check_stop_recording(
+    hand_bones: Query<&Transform, (With<OpenXRTracker>, With<HandBone>)>,
+    hands_resource: Res<HandsResource>,
+    mut next_spell_state: ResMut<NextState<RecordingStatus>>,
+) {
+    let thumb_tip = hand_bones.get(hands_resource.left.thumb.tip).unwrap();
+    let index_tip = hand_bones.get(hands_resource.left.index.tip).unwrap();
+    let thumb_index_dist = (thumb_tip.translation - index_tip.translation).length();
+
+    if thumb_index_dist > 0.02 {
+        next_spell_state.set(RecordingStatus::Awaiting);
+    }
+}
+
+pub fn get_recognized_words<'a>(
+    clip: &mut VoiceClip,
+    recogniser: &'a mut Recognizer,
+) -> std::str::SplitWhitespace<'a> {
+    let mut averaged_channel_data: Vec<i16> = Vec::with_capacity(clip.data.len() / 2);
     for index in (1..clip.data.len()).step_by(2) {
         averaged_channel_data.push((clip.data[index - 1] + clip.data[index]) / 2);
     }
     clip.data.clear();
 
-    recogniser.0.accept_waveform(&averaged_channel_data);
-    let result: CompleteResultSingle = recogniser
-        .0
+    recogniser.accept_waveform(&averaged_channel_data);
+    let result = recogniser
         .final_result()
         .single()
         .expect("Expect a single result, got one with alternatives");
-    let last_word = result.text.split_whitespace().last().unwrap_or("");
 
-    let (next_s, s_spell) = match last_word {
-        "fireball" => (SpellStatus::Armed, Some(Spell::Fireball)),
-        "lightning" => (SpellStatus::Armed, Some(Spell::Lightning)),
-        _ => (SpellStatus::None, None),
-    };
-    next_spell_state.set(next_s);
-    selected_spell.0 = s_spell;
+    result.text.split_whitespace()
 }
