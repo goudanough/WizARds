2ccbf0aa8dd89f8004d246a5309de3ed39949a41
lichengwei <ue19110@bristol.ac.uk>
2024-03-22 13:11:27 +0000

    fit bomb spell

--- src/network.rs
+++ src/network.rs
@@ -209,65 +209,7 @@ fn spawn_networked_player_objs(mut commands: Commands, args: Res<ConnectionArgs>
                 PlayerLeftPalm,
             ))
             .add_rollback();
-    
-    
-    // let mut color_gradient1 = Gradient::new();
-    // color_gradient1.add_key(0.0, Vec4::new(4.0, 4.0, 4.0, 1.0));
-    // color_gradient1.add_key(0.1, Vec4::new(4.0, 4.0, 0.0, 1.0));
-    // color_gradient1.add_key(0.9, Vec4::new(4.0, 0.0, 0.0, 1.0));
-    // color_gradient1.add_key(1.0, Vec4::new(4.0, 0.0, 0.0, 0.0));
 
-    // let mut size_gradient1 = Gradient::new();
-    // size_gradient1.add_key(1.0, Vec2::splat(0.1));
-    // size_gradient1.add_key(0.3, Vec2::splat(0.1));
-    // size_gradient1.add_key(0.0, Vec2::splat(0.));
-
-    // let writer = ExprWriter::new();
-
-    // // Give a bit of variation by randomizing the age per particle. This will
-    // // control the starting color and starting size of particles.
-    // let age = writer.lit(0.).uniform(writer.lit(0.5)).expr();
-    // let init_age = SetAttributeModifier::new(Attribute::AGE, age);
-
-    // // Give a bit of variation by randomizing the lifetime per particle
-    // let lifetime = writer.lit(0.5).expr();
-    // let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
-
-    // // Add drag to make particles slow down a bit after the initial explosion
-    // let drag = writer.lit(5.).expr();
-    // let update_drag = LinearDragModifier::new(drag);
-
-    // let init_pos = SetPositionSphereModifier {
-    // center: writer.lit(Vec3::ZERO).expr(),
-    // radius: writer.lit(0.).uniform(writer.lit(0.001)).expr(),
-    // dimension: ShapeDimension::Surface,
-    // };
-
-    // let init_size = SetSizeModifier {
-    //     size: bevy_hanabi::CpuValue::Single(Vec2 { x: 0.0001, y: 0.0001 }),
-    //     screen_space_size: false,
-    // };
-
-    // let effect = EffectAsset::new(
-    //     32768,
-    //     Spawner::new(2000.0.into(), 0.5.into(), 0.7.into()),
-    //     writer.finish(),
-    // )
-    // .with_name("sparkle")
-    // .init(init_pos)
-    // .init(init_age)
-    // .init(init_lifetime)
-    // .render(init_size)
-    // .update(update_drag)
-    // .render(ColorOverLifetimeModifier {
-    //     gradient: color_gradient1,
-    // })
-    // .render(SizeOverLifetimeModifier {
-    //     gradient: size_gradient1,
-    //     screen_space_size: false,
-    // });
-
-    // let effect1 = effects.add(effect);
         
         commands
             .spawn((
